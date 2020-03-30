114343ae56650a84942923ada8328c887bbee932
hzhero1 <38153901+hzhero1@users.noreply.github.com>
2020-03-30 16:34:20 +0100

    change

--- features.py
+++ features.py
@@ -51,29 +51,29 @@ def count_prob_words():
     save_json('json_data/words_prob.json', words_appearance_prob)
 
 
-def count_joint_prob():
-    vocab = load_json('json_data/vocab.json')
-    data = load_json('json_data/reut2.json')
-    num_of_data = 0
-    words_appearance_prob = defaultdict(float)
-    words_appearance_count = defaultdict(int)
-
-    for key in data.keys():
-        catagories = data.get(key)[1]
-        if catagories:
-            num_of_data += 1
-    print(num_of_data)
-
-    for word in vocab.keys():
-        for key in data.keys():
-            catagories = data.get(key)[1]
-            if catagories:
-                if word in data.get(key)[2].split():
-                    words_appearance_count[word] += 1
-    for word in words_appearance_count.keys():
-        words_appearance_prob[word] += words_appearance_count[word] / num_of_data
-
-    save_json('json_data/words_prob.json', words_appearance_prob)
+# def count_joint_prob():
+#     vocab = load_json('json_data/vocab.json')
+#     data = load_json('json_data/reut2.json')
+#     num_of_data = 0
+#     words_appearance_prob = defaultdict(float)
+#     words_appearance_count = defaultdict(int)
+#
+#     for key in data.keys():
+#         catagories = data.get(key)[1]
+#         if catagories:
+#             num_of_data += 1
+#     print(num_of_data)
+# 
+#     for word in vocab.keys():
+#         for key in data.keys():
+#             catagories = data.get(key)[1]
+#             if catagories:
+#                 if word in data.get(key)[2].split():
+#                     words_appearance_count[word] += 1
+#     for word in words_appearance_count.keys():
+#         words_appearance_prob[word] += words_appearance_count[word] / num_of_data
+#
+#     save_json('json_data/words_prob.json', words_appearance_prob)
 
 
 if __name__ == "__main__":
