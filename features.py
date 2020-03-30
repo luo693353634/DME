f50299281570ad3cfa778edfbb46de6ffb6d7062
hzhero1 <38153901+hzhero1@users.noreply.github.com>
2020-03-30 23:49:02 +0100

    compute p(x,c)

--- features.py
+++ features.py
@@ -6,17 +6,9 @@ def get_features():
     word_dict = word_count()
 
 
-def count_prob_categories():
+def count_prob_categories(num_of_data):
     listcat = count_categories()
     prob_of_category = defaultdict(float)
-    num_of_data = 0
-    data = load_json('json_data/reut2.json')
-
-    for key in data.keys():
-        catagories = data.get(key)[1]
-        if catagories:
-            num_of_data += 1
-    print(num_of_data)
 
     for i, item in enumerate(listcat):
         if i < 10:
@@ -26,18 +18,11 @@ def count_prob_categories():
     print(prob_of_category)
 
 
-def count_prob_words():
+def count_prob_words(num_of_data):
     vocab = load_json('json_data/vocab.json')
     data = load_json('json_data/reut2.json')
-    num_of_data = 0
     words_appearance_prob = defaultdict(float)
     words_appearance_count = defaultdict(int)
-
-    for key in data.keys():
-        catagories = data.get(key)[1]
-        if catagories:
-            num_of_data += 1
-    print(num_of_data)
 
     for word in vocab.keys():
         for key in data.keys():
@@ -51,31 +36,41 @@ def count_prob_words():
     save_json('json_data/words_prob.json', words_appearance_prob)
 
 
-# def count_joint_prob():
-#     vocab = load_json('json_data/vocab.json')
-#     data = load_json('json_data/reut2.json')
-#     num_of_data = 0
-#     words_appearance_prob = defaultdict(float)
-#     words_appearance_count = defaultdict(int)
-#
-#     for key in data.keys():
-#         catagories = data.get(key)[1]
-#         if catagories:
-#             num_of_data += 1
-#     print(num_of_data)
-# 
-#     for word in vocab.keys():
-#         for key in data.keys():
-#             catagories = data.get(key)[1]
-#             if catagories:
-#                 if word in data.get(key)[2].split():
-#                     words_appearance_count[word] += 1
-#     for word in words_appearance_count.keys():
-#         words_appearance_prob[word] += words_appearance_count[word] / num_of_data
-#
-#     save_json('json_data/words_prob.json', words_appearance_prob)
+def count_joint_prob(num_of_data):
+    vocab = load_json('json_data/vocab.json')
+    data = load_json('json_data/reut2.json')
+    catas = load_json('json_data/catas.json')
+    joint_count_dict = defaultdict(lambda: defaultdict(int))
+    joint_prob_dict = defaultdict(lambda: defaultdict(float))
+
+    cat_list = []
+    for i, c in enumerate(catas.keys()):
+        if i < 10:
+            cat_list.append(c)
+
+    for cat in cat_list:
+        for word in vocab.keys():
+            for key in data.keys():
+                if cat in data.get(key)[1].split():
+                    if word in data.get(key)[2].split():
+                        joint_count_dict[word][cat] += 1
+    for word in joint_count_dict.keys():
+        for cat in joint_count_dict[word].keys():
+            joint_prob_dict[word][cat] = joint_count_dict[word][cat] / num_of_data
+
+    save_json('json_data/joint_prob.json', joint_prob_dict)
 
 
 if __name__ == "__main__":
-    count_prob_categories()
-    count_prob_words()
+    data = load_json('json_data/reut2.json')
+    num_of_data = 0
+
+    for key in data.keys():
+        catagories = data.get(key)[1]
+        if catagories:
+            num_of_data += 1
+    print(num_of_data)
+
+    count_joint_prob(num_of_data)
+    # count_prob_categories()
+    # count_prob_words()
